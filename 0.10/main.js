d3324c98ae3ea5da412023a5816e9a378d1032f6
gaohang <isaackao@foxmail.com>
2018-10-28 18:35:02 +0800

    filter过滤器

--- 0.10/main.js
+++ 0.10/main.js
@@ -1,20 +1,20 @@
 //过滤器
-Vue.filter('meter', function(val, unit) {
-  val = val ? val : 0
-  unit = unit? unit : 'm'
-  return (val / 1000).toFixed(1) + unit
+Vue.filter('meter', function (val, unit) {
+    val = val ? val : 0
+    unit = unit ? unit : 'm'
+    return (val / 1000).toFixed(1) + unit
 })
 
-Vue.filter('currency', function(val, unit) {
-  val = val ? val : 0
-  unit = unit ? unit : '元'
-  return val + unit
+Vue.filter('currency', function (val, unit) {
+    val = val ? val : 0
+    unit = unit ? unit : '元'
+    return val + unit
 })
 
 new Vue({
-  el: "#app",
-  data: {
-    price: 10,
-    length: 10
-  }
+    el: "#app",
+    data: {
+        price: 10,
+        length: 10
+    }
 })
