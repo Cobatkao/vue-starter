d212408d844a0ceff0fd4c0159f612fc456aaa3d
gaohang <isaackao@foxmail.com>
2018-10-28 17:47:25 +0800

    自定义指令 -filters基础配置

---
+++ 0.10/main.js
@@ -0,0 +1,19 @@
+Vue.filter('meter', function(val, unit) {
+  val = val ? val : 0
+  unit = unit? unit : 'm'
+  return (val / 1000).toFixed(1) + unit
+})
+
+Vue.filter('currency', function(val, unit) {
+  val = val ? val : 0
+  unit = unit ? unit : '元'
+  return val + unit
+})
+
+new Vue({
+  el: "#app",
+  data: {
+    price: 10,
+    length: 10
+  }
+})
